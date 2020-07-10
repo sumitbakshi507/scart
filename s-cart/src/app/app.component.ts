564543662337b46c3485def26ddf1005a4d89eb2
sumitbakshi507 <49333677+sumitbakshi507@users.noreply.github.com>
2020-07-10 21:11:05 +0530

    login page and services

--- s-cart/src/app/app.component.ts
+++ s-cart/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
+import { AuthService } from './services/auth.service';
+import { User } from './models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 's-cart';
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'Welcome to SCart';
+  userSubs: Subscription;
+  isAuthenticated: boolean;
+
+  constructor(
+    private titleService: Title,
+    private route: Router,
+    private authService: AuthService) {
+  }
+
+  ngOnInit() {
+      this.authService.autoLogin();
+      this.titleService.setTitle(this.title);
+      this.userSubs = this.authService.user.subscribe((user: User) => {
+        this.isAuthenticated = !!user;
+        if (!this.isAuthenticated) {
+          this.route.navigate(['auth']);
+        }
+        if (this.isAuthenticated) {
+          this.route.navigate(['']);
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.userSubs) {
+      this.userSubs.unsubscribe();
+    }
+  }
 }
